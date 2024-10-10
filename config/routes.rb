3b06c5a24e81e8d913ef0e15ede05a90595b08f7
加藤真優子 <>
2024-10-10 22:41:44 +0900

    投稿一覧ページ作成とtailwindの追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
 
   root to: 'top#index'
+  resources :posts
   # Defines the root path route ("/")
   # root "posts#index"
 end
