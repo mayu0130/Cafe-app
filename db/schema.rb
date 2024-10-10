91579e5ce027f1baf17492a845c0ede39b12060f
加藤真優子 <>
2024-10-10 15:57:17 +0900

    Postsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_010701) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_10_065212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "cafe_name", null: false
+    t.text "body", null: false
+    t.string "address", null: false
+    t.string "cafe_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
