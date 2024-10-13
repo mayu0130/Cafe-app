a12c06f524b802f1db8beab72dc3feac3a06ea4e
加藤真優子 <>
2024-10-13 10:46:23 +0900

    プロフィール編集画面とtailwindの追加

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,11 @@
 class Profile < ApplicationRecord
+   validates :nickname, presence: true, length: { maximum: 50 }
+   validates :birthday, presence: true
+   validates :mbti, format: { with: /\A[A-Z]{4}\z/, message: 'は4文字のMBTIタイプである必要があります' }, allow_blank: true
+   validates :address, length: { maximum: 100 }, allow_blank: true
+   validates :introduction, presence: true, length: { maximum: 500 }
+   validates :x_link, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'は有効なURLである必要があります' }, allow_blank: true
+   validates :instagram_link, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'は有効なURLである必要があります' }, allow_blank: true
+
   belongs_to :user
 end
