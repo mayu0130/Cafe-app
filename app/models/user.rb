9bf904456e1189bec8c5082e8ff0d06ad9117a91
加藤真優子 <>
2024-10-14 13:21:28 +0900

    リレーションの追加とブックマークする機能を管理するメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one :profile, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_posts, through: :bookmarks, source: :post
 
   def display_name
     self.email.split('@').first
@@ -26,5 +28,16 @@ class User < ApplicationRecord
     else
       'profile.png'
     end
+  end
+  def bookmark(post)
+    bookmark_posts << post
+  end
+
+  def unbookmark(post)
+    bookmark_posts.destroy(post)
+  end
+
+  def bookmark?(post)
+    bookmark_posts.include?(post)
   end
 end
