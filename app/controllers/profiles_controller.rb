934db82dc4352c54e6a3a33e5f8b22bf1e7bd134
加藤真優子 <>
2024-10-16 18:13:08 +0900

    プロフィールコントローラの修正

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,8 +13,6 @@ class ProfilesController < ApplicationController
 
   def update
     @profile = current_user.build_profile(profile_params)
-    @profile = current_user.prepare_profile
-    @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to profile_path, notice: 'プロフィールを更新しました'
     else
