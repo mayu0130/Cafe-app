c4e7c391878e58b35c0ac045baa530bb1d6a17df
加藤真優子 <>
2024-11-14 09:57:55 +0900

    住所が詳細になっても地図が表示されるよう記述

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,11 +1,24 @@
 Geocoder.configure(
-  lookup: :google,
+  # Geocoding options
+  # timeout: 3,                 # geocoding service timeout (secs)
+   lookup: :google,         # name of geocoding service (symbol)
+  # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
+  # language: :en,              # ISO-639 language code
+   use_https: true,           # use HTTPS for lookup requests? (if supported)
+  # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
+  # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
 
-  api_key: Rails.application.credentials[:map_api_key],
+#YOUR_API_KEYにはご自身のAPIキーを記述してください。
+   api_key: ENV['GOOGLE_MAP_API_KEY'] ,               # API key for geocoding service
+  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
+  # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
-  language: :ja,
+  # Exceptions that should not be rescued by default
+  # (if you want to implement custom error handling);
+  # supports SocketError and Timeout::Error
+  # always_raise: [],
 
-  timeout: 5,
-
-  units: :km
+  # Calculation options
+  # units: :mi,                 # :km for kilometers or :mi for miles
+  # distances: :linear          # :spherical or :linear
 )
