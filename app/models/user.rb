ce4132898384160ba0eb62bfdd8a91b27585b7e1
加藤真優子 <>
2024-11-17 10:23:00 +0900

    リレーションの記述とフォロー機能・アンフォロー機能の記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :bookmark_posts, through: :bookmarks, source: :post
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def display_name
     self.email.split('@').first
   end
@@ -39,5 +45,30 @@ class User < ApplicationRecord
 
   def bookmark?(post)
     bookmark_posts.include?(post)
+  end
+
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
+
+  private
+
+  def get_user_id(user)
+    if user.is_a?(User)
+      user.id
+    else
+      user
+    end
   end
 end
