2fd0a0afe72b23d8200a5da5f7a73563fe5e2faf
加藤真優子 <>
2024-11-18 11:24:51 +0900

    MBTIカラムのエラーメッセージの文面修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
    validates :nickname, presence: true, length: { maximum: 50 }
-   validates :mbti, format: { with: /\A[A-Z]{4}\z/, message: 'は4文字のMBTIタイプである必要があります' }, allow_blank: true
+   validates :mbti, format: { with: /\A[A-Z]{4}\z/, message: '英語の大文字4文字（例: INFP, ESTJ）」で入力してくださいgit' }, allow_blank: true
    validates :address, length: { maximum: 100 }, allow_blank: true
    validates :introduction, presence: true, length: { maximum: 500 }
    validates :x_link, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: 'は有効なURLである必要があります' }, allow_blank: true
