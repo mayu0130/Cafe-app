65b017eba568da2e93d1e02b07d703bbf16665e9
加藤真優子 <>
2025-01-18 11:27:39 +0900

    エラー解消 403: disallowed_useragen

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,6 +272,12 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  {
+    prompt: 'select_account',
+    access_type: 'offline',
+    hd: '',
+    provider_ignores_state: true
+  }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
