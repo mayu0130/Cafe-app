7c899b7afdbb318972ce4084dc18fc6e2a55932d
加藤真優子 <>
2025-02-09 17:34:19 +0900

    他ユーザのプロフィールページにページネーションを追加

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,7 +4,7 @@ class AccountsController < ApplicationController
   def show
     @user = User.find(params[:id])
     @profile = @user.profile
-    @posts = @user.posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(8)
     if @user == current_user
       redirect_to profile_path
     end
