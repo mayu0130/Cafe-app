e658a1ed4f2933d3126272ccd47dac0cf0621135
加藤真優子 <>
2025-02-28 18:30:27 +0900

    いいね機能のマイグレーションファイルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_08_025027) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_28_044618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_08_025027) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "used_date"], name: "index_daily_usage_counts_on_user_id_and_used_date", unique: true
     t.index ["user_id"], name: "index_daily_usage_counts_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
